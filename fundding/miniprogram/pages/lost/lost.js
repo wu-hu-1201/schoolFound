0754d99a7baf1ac595fa1834d491dfe2db8b04c0
735940104@qq.com <735940104@qq.com>
2020-09-13 19:27:52 +0800

    up

--- fundding/miniprogram/pages/lost/lost.js
+++ fundding/miniprogram/pages/lost/lost.js
@@ -5,6 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
+    LostList: [
+      {
+        img: '../../images/sort-u.png',
+        kind: 'lost',
+        title: '寻找遗失的雨伞',
+        categoryTag: '其他',
+        intro:'今天下午两代年左右弄丢了这把黑色雨伞,有好心人捡到能通知我一声',
+        upName: '金龙',
+        date: '2020/9/9',
+        avat: '../../images/sort-u.png',
+      },
+      {
+        img: '../../images/sort-u.png',
+        kind: 'lost',
+        title: '寻找遗失的雨伞asdasdasdaaaaaa',
+        categoryTag: '其他',
+        intro:'今天下午两代年左右弄丢了这把黑色雨伞,有好心人捡到能通知我一声',
+        upName: '金龙',
+        date: '2020/9/9',
+        avat: '../../images/sort-u.png',
+      },
+      {
+        img: '../../images/sort-c.png',
+        kind: 'lost',
+        title: '寻找遗失的雨伞',
+        categoryTag: '证件',
+        intro:'今天下午两代年左右弄丢了这把黑色雨伞,有好心人捡到能通知我一声',
+        upName: '金龙',
+        date: '2020/9/9',
+        avat: '../../images/sort-u.png',
+      },
+      {
+        img: '../../images/sort-bag.png',
+        kind: 'lost',
+        title: '寻找遗失的雨伞',
+        categoryTag: '贵重物品',
+        intro:'今天下午两代年左右弄丢了这把黑色雨伞,有好心人捡到能通知我一声',
+        upName: '金龙',
+        date: '2020/9/9',
+        avat: '../../images/sort-u.png',
+      },
+      {
+        img: '../../images/sort-b.png',
+        kind: 'book',
+        title: '寻找遗失的雨伞',
+        categoryTag: '书籍',
+        intro:'今天下午两代年左右弄丢了这把黑色雨伞,有好心人捡到能通知我一声',
+        upName: '金龙',
+        date: '2020/9/9',
+        avat: '../../images/sort-u.png',
+      }
+    ],
     categoryList: [
       '全部',
       '证件',
@@ -12,11 +64,28 @@ Page({
       '贵重物品',
       '其他'
     ],
-    isClick: 0
+    clickNum: 0,
+    selectCategory: []
+    
   },
+  // 点击展示不同类型的物品
   clickSelected: function (e) {
+    if( !this.data.LostList) {
+      return
+    }
+    let array = []
+    this.data.LostList.forEach((item) => {
+      if(e.target.dataset.num != 0){
+        if(item.categoryTag === this.data.categoryList[e.target.dataset.num]  ) {
+          array.push(item)
+        }
+      } else {
+        array.push(item)
+      }
+    })
     this.setData({
-      isClick : e.target.dataset.num
+      clickNum: e.target.dataset.num,
+      selectCategory: array
     })
   },
 
@@ -38,7 +107,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setData({
+      selectCategory: this.data.LostList
+    })
   },
 
   /**
